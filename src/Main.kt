92cce51b0a2284430817637783e71b7774a90e7b
thiagofarias-dev <thiagofarias13@hotmail.com>
2024-04-15 21:32:10 -0300

    Start

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val set = hashSetOf(1, 7, 53)
+    val list = arrayListOf(1, 7, 63)
+    val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
+
+    println(set.javaClass)
+
+    println(list.javaClass)
+
+    println(map.javaClass)
+
+}
