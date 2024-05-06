afc53590cb2d2283b0557b8b8f3ad387456aee35
thiagofarias-dev <thiagofarias13@hotmail.com>
2024-05-06 20:32:55 -0300

    Example 3.2 - Adicionado nova forma de chamar o joinToString

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+
 fun main() {
     example1()
     println()
@@ -26,8 +27,8 @@ fun example2(){
     println(numbers.max()) //.max() retorna o maior valor na lista
 }
 
-//Cap 3.2
-fun <T> joinToString( //Essa function implementa uma saída toString personalizada
+//Cap 3.1 function abaixo comentada para não sobrescrever o outro modo de escrever
+/* fun <T> joinToString( //Essa function implementa uma saída toString personalizada (modo 1)
     collection: Collection<T>,
     separator: String,
     prefix: String,
@@ -41,14 +42,39 @@ fun <T> joinToString( //Essa function implementa uma saída toString personaliza
     }
     result.append(postfix)                              //adiciona o parentese após o último elemento da lista
     return result.toString()
-}
+} */
 
 fun example3(){
     val list = listOf(1,2,3)
     println(listOf(list)) // modo padrão de listar uma string
     println(joinToString(list, ";", "(", ")"))
     println(joinToString(list, separator = " ", prefix = " ", postfix = ".")) // outra maneira de chamar o joinToString ficar mais legível
+    println()
+    println(joinToString(list, ", ", "", "")) //modo 2 - passando todos os parâmetros
+    println(joinToString(list))                                       //modo 2 - não passando parâmetros
+    println(joinToString(list,"; "))                        //modo 2 - passando apenas parâmetro de separador
+    println(joinToString(list, postfix = ";", prefix = "# "))        //modo 2 - passando parâmetros em ordem diferente e omitindo um deles
 
 }
+
+//Cap 3.2
+fun <T> joinToString( //Essa function implementa uma saída toString personalizada (modo 2 - utilizando argumentos nomeados, possibilita a omissão de parâmetros durante a chamada da function e também a troca de ordem dos argumentos )
+    collection: Collection<T>,
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)                  //adiciona o parentese antes do primeiro elemento da lista
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)         //não adiciona separados antes do primeiro elemento da lista
+        result.append(element)
+
+    }
+    result.append(postfix)                              //adiciona o parentese após o último elemento da lista
+    return result.toString()
+}
+
+
+
 
 
