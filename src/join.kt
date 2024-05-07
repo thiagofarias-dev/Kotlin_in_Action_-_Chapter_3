2bf889c4427d49b7dd6b6bdc08ea872479df710c
thiagofarias-dev <thiagofarias13@hotmail.com>
2024-05-06 21:31:38 -0300

    Example 3.2 - criado pacote com a function e importado no arquivo Main

---
+++ src/join.kt
@@ -0,0 +1,17 @@
+package strings
+//Cap 3.2
+fun <T> joinToString( //Essa function implementa uma saída toString personalizada (modo 2 - utilizando argumentos nomeados, possibilita a omissão de parâmetros durante a chamada da function e também a troca de ordem dos argumentos )
+    collection: Collection<T>,
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)                  //adiciona o parentese antes do primeiro elemento da lista
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)         //não adiciona separados antes do primeiro elemento da lista
+        result.append(element)
+
+    }
+    result.append(postfix)                              //adiciona o parentese após o último elemento da lista
+    return result.toString()
+}
